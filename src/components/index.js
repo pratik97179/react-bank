5daf470ef98eb3682641be334fae2db700906d6b
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-08 07:00:39 +0530

    Basic file structuring finished

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Billing from "./Billing.jsx";
 import CardDeal from "./CardDeal.jsx";
 import Business from "./Business.jsx";
 import Clients from "./Clients.jsx";
-import CTA from "./CTA.jsx";
+import Cta from "./Cta.jsx";
 import Stats from "./Stats.jsx";
 import Footer from "./Footer.jsx";
 import Testimonials from "./Testimonials.jsx";
@@ -15,7 +15,7 @@ export {
   CardDeal,
   Business,
   Clients,
-  CTA,
+  Cta,
   Stats,
   Footer,
   Testimonials,
