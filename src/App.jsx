3cb37833bea30890d606fe8edd16fa94526f8480
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-08 06:21:35 +0530

    Basic styling file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return <div className="bg-primary w-full overflow-hidden"></div>;
+};
 
 export default App;
