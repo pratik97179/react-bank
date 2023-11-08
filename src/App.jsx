d74b69adeb8c3c3393efe8ee3818f325ed2f8898
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-08 06:59:13 +0530

    Basic file structuring finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import styles from "./styles.js";
+
+import {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    Cta,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+} from "./components/index.js";
 
 const App = () => {
-    return <div className="bg-primary w-full overflow-hidden"></div>;
+    return (
+        <div className="bg-primary w-full overflow-hidden">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Navbar />
+                </div>
+            </div>
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Hero />
+                </div>
+            </div>
+            <div
+                className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}
+            >
+                <div className={`${styles.boxWidth}`}>
+                    <Stats />
+                    <Business />
+                    <Billing />
+                    <CardDeal />
+                    <Testimonials />
+                    <Clients />
+                    <Cta />
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
