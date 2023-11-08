6d06bc5e2b2ad4719ff47c1649053a25c6c58bb6
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-08 23:01:47 +0530

    Business section finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ styles }) => (
+    <button
+        type="button"
+        className={`font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}
+    >
+        Get Started
+    </button>
+);
 
-export default Button
+export default Button;
