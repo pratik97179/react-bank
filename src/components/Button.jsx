f98ef7e550c5c1e140ee2f8c27da8421fe13cbb2
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-09 16:49:33 +0530

    Card deal section finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ styles }) => (
     <button
         type="button"
-        className={`font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}
+        className={`font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles} rounded-[10px]`}
     >
         Get Started
     </button>
